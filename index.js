92945b1c6af9060a02ebe8b28ca6046618f8644c
Rabie TAWIL <rabietawil@gmail.com>
2019-07-02 20:44:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function distanceTravelledInFeet(start, destination){
 }
 
 function calculatesFarePrice(start, destination){
+  let distance = distanceTravelledInFeet(start, destination)
   if (distanceTravelledInFeet(start, destination) <= 400 ){
     return price = "gives customers a free sample";
   }
